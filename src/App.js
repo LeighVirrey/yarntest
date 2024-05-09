1d1baa784b4fee5ce7ae81185fa0a77b54eda073
TOONZK <126704941+LeighVirrey@users.noreply.github.com>
2024-05-08 18:50:21 -0600

    facemesh

--- src/App.js
+++ src/App.js
@@ -6,9 +6,50 @@ import './App.css';
 import React, { useRef } from 'react';
 
 function App() {
+  //useref for webacam and canvas
   const webcamRef = useRef(null)
   const canvasRef = useRef(null)
 
+  const runFacemesh = async () => {
+    //loading facemesh
+    const net = await facemesh.load({
+      inputResolution: { width: 640, height: 480 },
+      scale: 0.8
+    })
+    //this set interval detects every 100ms and after detecting it runs the detect function
+    setInterval(() => {
+      detect(net)
+    }, 100)
+  }
+
+  //detect function, detects face and starts running
+  const detect = async (net) => {
+    //check if webcam is available
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+      //get video properties
+      const video = webcamRef.current.video
+      const videoWidth = webcamRef.current.video.videoWidth
+      const videoHeight = webcamRef.current.video.videoHeight
+
+      //set video properties
+      webcamRef.current.video.width = videoWidth
+      webcamRef.current.video.height = videoHeight
+
+      //set canvas properties
+      canvasRef.current.width = videoWidth
+      canvasRef.current.height = videoHeight
+
+      //make detections
+      const face = await net.estimateFaces(video)
+      console.log(face)
+
+      //get canvas context
+      const ctx = canvasRef.current.getContext("2d")
+      drawMesh(face, ctx)
+    }
+  }
+
+  runFacemesh()
   return (
     <div className="App">
       <Webcam ref={webcamRef} style={{
